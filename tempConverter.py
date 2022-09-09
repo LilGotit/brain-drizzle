4d1cd4a3bb9f03e0ba5a90181e9587a269ce9262
LilGotit <jbeld0002@kctcs.edu>
2022-09-08 21:57:53 -0400

    Finished Temperature Converter project for Python class

--- tempConverter.py
+++ tempConverter.py
@@ -1,12 +1,11 @@
-# Joshua Beld - CIT 144: Python - Programming Project 1
 startTemp = input("Enter numeric temperature value to convert: ")
 endFormat = input("Convert to (C) Celsius or (F) Farenheit? ")
 
 if endFormat == "C" or endFormat == "c":
     degC = (float(startTemp) - 32) / 1.8
-    print (str(startTemp) + " degrees Farenheit is equal to " + str(degC) + " degrees Celsius")
+    print (str(startTemp) + " degrees Farenheit is equal to " + str(round(degC, 1)) + " degrees Celsius")
 elif endFormat == "F" or endFormat == "f":
     degF = (1.8 * float(startTemp)) + 32
-    print(str(startTemp) + " degrees Celsius is equal to " + str(degF) + " degrees Farenheit")
+    print(str(startTemp) + " degrees Celsius is equal to " + str(round(degF, 1)) + " degrees Farenheit")
 else:
     print("You did not enter an F or C, goodbye.")
