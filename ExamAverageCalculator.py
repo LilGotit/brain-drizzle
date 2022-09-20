d13c69b3e1330a52c20d8b3f2aa3b3da6a2e01e2
LilGotit <jbeld0002@kctcs.edu>
2022-09-20 15:05:45 -0400

    Smoothed out second project for Python class

--- ExamAverageCalculator.py
+++ ExamAverageCalculator.py
@@ -3,13 +3,13 @@ examTotal = 0
 score = 0
 while score != 5309:
     score = float(input("Enter a number. 5309 to quit: "))
-    if score < 0 or score > 100:
+    if score == 5309:
+        break
+    elif score < 0 or score > 100:
         print("Incorrect score entered; re-enter actual exam score: ")
         continue
-    elif score == 5309:
-        break
     else:
         examTotal += score
         examCount += 1
 examAverage = examTotal / examCount
-print("These " + str(examCount) + " scores average out to " + str(examAverage))
+print("These " + str(examCount) + " scores produce an average score, when rounded to a single decimal digit, of " + str(round(examAverage, 1)) + " per exam.")
