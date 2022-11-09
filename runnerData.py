6f0cb7160ce846978b5721dbd454a1b80403ef58
LilGotit <jbeld0002@kctcs.edu>
2022-11-09 02:15:41 -0500

    new Python class project just dropped - runnerData

---
+++ runnerData.py
@@ -0,0 +1,16 @@
+import csv
+
+def displayAll():
+    print("help")
+
+def displayRunner():
+    print("More help")
+
+def addRunner():
+    print("egg")
+
+def saveAndExit():
+    print("Store")
+
+def reader():
+    csv.DictReader()
